714c0e9209a90fe2b4341b43b674068278ccd2f7
yeqiu741 <1428114973@qq.com>
2018-08-22 19:07:40 +0800

    测试

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 import registerServiceWorker from './registerServiceWorker';
-import App from './App'
+import App from './App';
 
 const store = configureStore();
 
